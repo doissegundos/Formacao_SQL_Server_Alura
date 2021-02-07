1a2c1b102f5ad4ca1cdab2dd3467a9c250190f65
adna <stefaneadnas@gmail.com>
2021-02-07 10:37:29 -0300

    add registros nas tabelas

---
+++ Manipulação de dados com Microsoft SQL Server 2017/SQLQuery2.sql
@@ -0,0 +1,9 @@
+/****** Script do comando SelectTopNRows de SSMS  ******/
+SELECT TOP (1000) [MATRÍCULA]
+      ,[NOME]
+      ,[BAIRRO]
+      ,[COMISSÃO]
+      ,[DATA ADMISSÃO]
+      ,[FÉRIAS]
+  FROM [VENDAS SUCOS].[dbo].[VENDEDORES]
+
